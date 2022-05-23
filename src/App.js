fe5d2e5649e874f1545598aed036bbdd67257cce
Jonathan <jonato96s@gmail.com>
2022-05-23 11:37:46 -0500

    Primer Commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="assets/bot.png" className="App-logo" alt="logo" />
+        <p>
+          Soy <code>Jonathan</code>, estoy probando React.
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/jonato96"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Mi Github
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
